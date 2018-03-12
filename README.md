71ec7e502bba639ede4c07222a7808caf23155df
hikecoder <hikecoder@gmail.com>
2018-03-12 23:12:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# ERC20 Token Generator
+
+
+The source code for ERC20 Token Generator DApp. 
+
+
+Simply deploy Smart Contract for a Standard, Mintable, Burnable ERC20 Token.
+
+
+Live here [https://vittominacori.github.io/erc20-generator](https://vittominacori.github.io/erc20-generator)
+
+
+
+## Download and Installation
+
+```bash
+git clone https://github.com/vittominacori/erc20-generator.git
+cd erc20-generator
+git checkout gh-pages
+gem install jekyll
+npm install
+gulp
+```
+
+
+
+## Usage
+
+### Run locally
+
+```bash
+bundle exec jekyll serve --config _config-dev.yml
+```
+
+
+Go to [http://localhost:4000](http://localhost:4000)
+
+
+
+### Build
+
+
+#### Gulp Tasks
+
+- `gulp` the default task that builds everything
+- `gulp sass` compiles SCSS files into CSS
+- `gulp minify-css` minifies the compiled CSS file
+- `gulp minify-js` minifies the themes JS file
+- `gulp watch` watches JS and SCSS files and makes previous tasks
+- `gulp copy` copies dependencies from node_modules to the vendor directory
+
+
+
+## Helpful Links
+ 
+Web3.js [Doc](http://web3js.readthedocs.io/en/1.0/index.html) [GitHub](https://github.com/ethereum/web3.js/)
+   
+Truffle Contract [GitHub](https://github.com/trufflesuite/truffle-contract)
+ 
+Jekyll [Doc](https://jekyllrb.com/docs/home/) [GitHub](https://github.com/jekyll/jekyll)
+
+
+
+## Bugs and Issues
+
+Have a bug? [Open a new issue](https://github.com/vittominacori/erc20-generator/issues).
+
+
+
+## Copyright and License
+
+Copyright 2018 @vittominacori. Code released under the [MIT](https://github.com/vittominacori/erc20-generator/blob/master/LICENSE) license.
---
+++ js/app.js
@@ -0,0 +1,118 @@
+const App = {
+    web3: null,
+    web3Provider: null,
+    etherscanLink: '',
+    metamask: {
+        installed: false,
+        netId: null
+    },
+    contracts: {},
+
+    init: function () {
+        App.initWeb3(true);
+    },
+
+    initWeb3: function (checkWeb3) {
+        App.etherscanLink = etherscanLink;
+        if (checkWeb3 && typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider;
+            App.web3 = new Web3(App.web3Provider);
+            App.metamask.installed = true;
+            App.web3.version.getNetwork(function (err, netId) {
+                App.metamask.netId = netId;
+                if (netId !== networkId) {
+                    App.initWeb3(false);
+                }
+            });
+        } else {
+            // set the provider you want from Web3.providers
+            App.web3Provider = new Web3.providers.HttpProvider(web3Provider);
+            App.web3 = new Web3(App.web3Provider);
+        }
+    },
+
+    initToken: async function () {
+        return $.getJSON(abiPath + 'ERC20Token.json', function (data) {
+            App.contracts.ERC20Token = TruffleContract(data);
+            App.contracts.ERC20Token.setProvider(App.web3Provider);
+        });
+    },
+
+    home: async function () {
+        App.init();
+
+        Vue.use(VeeValidate);
+        await App.initToken();
+
+        new Vue({
+            el: '#token-generator',
+            data: {
+                trxHash: '',
+                makingTransaction: false,
+                formDisabled: false,
+                token: {}
+            },
+            created: function () {
+                this.$validator.extend('eth_address', {
+                    getMessage: field => 'Insert a valid Ethereum wallet address.',
+                    validate: value => App.web3.isAddress(this.crowdsale.wallet)
+                });
+            },
+            methods: {
+                generateToken: function () {
+                    if (!App.metamask.installed) {
+                        alert("To create a Token please install the MetaMask extension!");
+                        return;
+                    } else {
+                        if (App.metamask.netId !== networkId) {
+                            alert("Your MetaMask extension in on the wrong network. Please switch on " + networkName + " and try again!");
+                            return;
+                        }
+                    }
+
+                    this.$validator.validateAll().then(async (result) => {
+                        if (result) {
+                            const name = this.token.name;
+                            const symbol = this.token.symbol;
+                            const decimals = new App.web3.BigNumber(this.token.decimals);
+
+                            try {
+                                this.trxHash = '';
+                                this.formDisabled = true;
+                                this.makingTransaction = true;
+
+                                const log = await App.contracts.ERC20Token.new(
+                                    name,
+                                    symbol,
+                                    decimals
+                                );
+
+                                this.token.address = log.address;
+                                this.trxHash = log.contract.transactionHash;
+                                this.trxLink = App.etherscanLink + "/tx/" + this.trxHash;
+                                this.token.link = App.etherscanLink + "/token/" + this.token.address;
+                            } catch (e) {
+                                this.makingTransaction = false;
+                                this.formDisabled = false;
+                                alert("Some error occurred. Maybe you rejected the transaction or you have MetaMask locked!");
+                            }
+                        } else {
+                            console.log("some errors");
+                        }
+                    });
+                }
+            }
+        });
+    },
+};
+
+(function($) {
+    "use strict";
+
+    switch (page) {
+        case "": //home
+            App.home();
+            break;
+    }
+
+})(jQuery); // End of use strict
