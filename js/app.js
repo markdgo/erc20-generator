88dff9e79d1d357ac6200c4e972c82cd88109e8c
hikecoder <hikecoder@gmail.com>
2018-03-15 19:53:51 +0100

    added link to metamask

--- js/app.js
+++ js/app.js
@@ -56,6 +56,7 @@ const App = {
             data: {
                 testnet: networkId !== "1",
                 networkName: networkName,
+                hasMetaMask: App.metamask.installed,
                 trxHash: '',
                 makingTransaction: false,
                 formDisabled: false,
