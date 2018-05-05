b23cc668d576ae1dd15fd2bcc8905cacbde96d59
hikecoder <hikecoder@gmail.com>
2018-05-05 17:46:26 +0200

    edited truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,31 @@
+require('babel-register');
+require('babel-polyfill');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    develop: {
+      host: 'localhost',
+      port: 9545,
+      network_id: '*', // Match any network id
+      gas: 6000000, // Gas limit used for deploys
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
 };
