5e1fddc8fdad3c12ca7e67bce815974256fd8473
hikecoder <hikecoder@gmail.com>
2018-06-14 14:40:21 +0200

    skip some files from coverage

--- .solcover.js
+++ .solcover.js
@@ -3,5 +3,8 @@ module.exports = {
   norpc: true,
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-  skipFiles: []
+  skipFiles: [
+    'Migrations.sol',
+    'mocks'
+  ]
 };
