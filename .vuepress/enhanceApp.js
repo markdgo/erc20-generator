1153a8f24fe1f683ca45f634f31668e424996974
hikecoder <hikecoder@gmail.com>
2018-10-20 20:41:03 +0200

    moved all to vue bootstrap

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,7 +1,5 @@
 import BootstrapVue from 'bootstrap-vue';
-import VeeValidate from 'vee-validate';
 
 export default ({ Vue }) => {
   Vue.use(BootstrapVue);
-  Vue.use(VeeValidate);
 };
--- .vuepress/theme/Footer.vue
+++ .vuepress/theme/Footer.vue
@@ -2,7 +2,7 @@
     <b-row>
         <b-col lg="8" offset-lg="2" class="mt-4 p-0">
             <b-link href="https://www.friendsfingers.com/?utm_campaign=github&utm_source=erc20-generator" target="_blank">
-                <img :src="$withBase('/assets/images/friends-fingers-banner.png')" class="img-fluid" alt="FriendsFingers">
+                <b-img fluid :src="$withBase('/assets/images/friends-fingers-banner.png')" alt="FriendsFingers"></b-img>
             </b-link>
         </b-col>
         <b-col lg="8" offset-lg="2" class="text-center text-white p-0">
@@ -14,7 +14,7 @@
                     <small>
                         GitHub:
                         <a class="text-light" href="https://github.com/vittominacori/erc20-generator" target="_blank">Smart Contract</a> -
-                        <a class="text-light" href="https://github.com/vittominacori/erc20-generator/tree/gh-pages" target="_blank">DApp</a>
+                        <a class="text-light" href="https://github.com/vittominacori/erc20-generator/tree/dapp" target="_blank">DApp</a>
                     </small>
                 </li>
             </ul>
