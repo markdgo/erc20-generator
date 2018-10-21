51388e109e4353d96548d1ea5376efd4599a0afd
hikecoder <hikecoder@gmail.com>
2018-10-21 12:30:06 +0200

    fix twitter title

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'ERC20 Token Generator | Build your Token for free',
+  title: 'ERC20 Token Generator | Build your Token for FREE',
   description: 'Simply deploy Smart Contract for a Standard, Mintable, Burnable, Payable ERC20 Token with Minter Role.',
   base: '/erc20-generator/',
   ga: 'UA-115756440-2',
@@ -10,6 +10,7 @@ module.exports = {
     ['meta', { property: 'og:image', content: '/erc20-generator/assets/images/erc20-token-generator.png' }],
     ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
     ['meta', { property: 'twitter:image', content: '/erc20-generator/assets/images/erc20-token-generator.png' }],
+    ['meta', { property: 'twitter:title', content: 'ERC20 Token Generator | Build your Token for FREE' }],
     ['script', { src: 'assets/js/web3.min.js' }],
   ],
 };
