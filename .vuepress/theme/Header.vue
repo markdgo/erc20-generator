c759c5b41c79ec01772ca946206b8f73876507c8
hikecoder <hikecoder@gmail.com>
2019-02-03 15:26:36 +0100

    add a donate link

--- .vuepress/theme/Header.vue
+++ .vuepress/theme/Header.vue
@@ -11,6 +11,7 @@
             </b-navbar-nav>
 
             <b-navbar-nav class="ml-auto">
+                <b-nav-item target="_blank" href="https://vittominacori.github.io/ethereum-badge/detail.html?address=0x158D6727B17aaBa726D7318ac7dcA5edf8fAf6f8">Donate</b-nav-item>
                 <b-nav-item-dropdown text="GitHub" right>
                     <b-dropdown-item target="_blank" href="https://github.com/vittominacori/erc20-generator">Smart Contracts</b-dropdown-item>
                     <b-dropdown-item target="_blank" href="https://github.com/vittominacori/erc20-generator/tree/dapp">DApp</b-dropdown-item>
