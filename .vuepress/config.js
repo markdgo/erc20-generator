d91b29f5555a264dac67d112f35b1811beef6e54
hikecoder <hikecoder@gmail.com>
2019-12-07 17:22:15 +0100

    updated vuepress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
   title: 'ERC20 Token Generator | Create your Token for FREE',
   description: 'Easily deploy Smart Contract for a Standard, Capped, Mintable, Burnable ERC20 Token.',
   base: '/erc20-generator/',
-  ga: 'UA-115756440-2',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { property: 'og:type', content: 'website' }],
@@ -12,6 +11,14 @@ module.exports = {
     ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/erc20-generator/assets/images/erc20-token-generator.png' }], // eslint-disable-line max-len
     ['meta', { property: 'twitter:title', content: 'ERC20 Token Generator | Create your Token for FREE' }],
     ['script', { src: 'assets/js/web3.min.js' }],
+  ],
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-115756440-2',
+      },
+    ],
   ],
   defaultNetwork: 'mainnet',
 };
