b2cecf30970b50f273a757d1089627f75c57b133
hikecoder <hikecoder@gmail.com>
2020-04-01 18:37:57 +0200

    remove covid banner

--- .vuepress/components/Generator.vue
+++ .vuepress/components/Generator.vue
@@ -1,20 +1,6 @@
 <template>
     <b-row>
         <b-col lg="10" offset-lg="1" class="mt-4 p-0">
-            <b-alert show variant="warning">
-                <h4 class="alert-heading">Help Italy against Coronavirus (COVID-19)</h4>
-                <p>
-                    We are experiencing an unprecedented moment in history and the lives of every one of us has been affected.
-                    Many of us are taking action to cope with an event that has so suddently struck the lives of the Italian people and many others across the globe.<br>
-                    Be present by entering the <b-badge variant="danger">#againstcoronavirus</b-badge> campaign.<br>
-                </p>
-                <hr>
-                <p>
-                    Be present, <b-link target="_blank" href="/cryptogift/against-coronavirus.html">Donate</b-link>
-                </p>
-            </b-alert>
-        </b-col>
-        <b-col lg="10" offset-lg="1" class="mt-4 p-0">
             <b-card bg-variant="light" v-if="!loading" :title="$site.title">
                 <p class="card-text">
                     {{ $site.description }}
