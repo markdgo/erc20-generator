eaab940a986f9bc4989058703c3a9916dfc606c1
hikecoder <hikecoder@gmail.com>
2020-12-31 19:48:33 +0100

    feat: update web console

--- dist/ServiceReceiver.dist.sol
+++ dist/ServiceReceiver.dist.sol
@@ -1,6 +1,6 @@
-// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
-
 // SPDX-License-Identifier: MIT
+
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
 
 pragma solidity ^0.7.0;
 
@@ -80,7 +80,7 @@ interface IERC20 {
 
 // File: @openzeppelin/contracts/GSN/Context.sol
 
-// SPDX-License-Identifier: MIT
+
 
 pragma solidity ^0.7.0;
 
@@ -107,7 +107,7 @@ abstract contract Context {
 
 // File: @openzeppelin/contracts/access/Ownable.sol
 
-// SPDX-License-Identifier: MIT
+
 
 pragma solidity ^0.7.0;
 
@@ -177,7 +177,7 @@ abstract contract Ownable is Context {
 
 // File: eth-token-recover/contracts/TokenRecover.sol
 
-// SPDX-License-Identifier: MIT
+
 
 pragma solidity ^0.7.0;
 
@@ -202,7 +202,7 @@ contract TokenRecover is Ownable {
 
 // File: contracts/service/ServiceReceiver.sol
 
-// SPDX-License-Identifier: MIT
+
 
 pragma solidity ^0.7.0;
 
